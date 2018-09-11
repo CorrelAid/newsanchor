a38647cc74f8a0662e6d6169098d027daaba9bb5
Unknown <friedrike.preu@uni-konstanz.de>
2018-09-11 23:28:11 +0200

    #6: first few tests

--- tests/testthat/test_get_everything.R
+++ tests/testthat/test_get_everything.R
@@ -1,9 +1,9 @@
-context("foo")
-testthat::test_that("get everything returns error if no argument provided", {
+context("Get everything")
+testthat::test_that("that that function returns error if no argument provided", {
   testthat::expect_error(newsanchor::get_everything())
 })
 
-testthat::test_that("test with api key", {
+testthat::test_that("test correct number of rows", {
   res <- newsanchor::get_everything(key = "457e40043f6b4418ab108e3eb11ccc1e", content = "Merkel")
-  testthat::expect_equal(nrow(res), 20)
+  testthat::expect_equal(nrow(res$results_df), 20)
 })
