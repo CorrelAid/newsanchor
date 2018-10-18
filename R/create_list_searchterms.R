9ec76cc3fa82529b9d5b9add86387553fa671bf3
Lars Schulze <schulze.lars@rocketmail.com>
2018-10-18 18:51:03 +0200

    add 'create_list_searchterms'

--- R/create_list_searchterms.R
+++ R/create_list_searchterms.R
@@ -4,71 +4,58 @@
 #' 'category', and news 'sources'. This is not intended to be distributed in
 #' the package, but rather allows to update the dataset of searchterms.
 #'
-#' @param api_key API key required to access 'sources'
 #' @importFrom xml2 read_html
 #' @importFrom rvest html_nodes html_text
 #' @importFrom devtools use_data
 #' @examples
-#' create_list_searchterms(api_key)
 #' create_list_searchterms()
 
-create_list_searchterms <- function(api_key = NULL){
+create_list_searchterms <- function(){
 
-  # check for api-key in the global environment
-  if (is.null(api_key)) {
-    api_key <- Sys.getenv("NEWS_API_KEY")
-  }
+  # access URL --------------------------------------------------------------
   
-  # is an api-key available?
-  if (nchar(api_key) == 0){
-    stop("Please provide an api-key with the function call or to the global
-         environment")
-  }
-  
-
-  # URL to access
+  # define URL
   url_headlines = "https://newsapi.org/docs/endpoints/top-headlines"
 
   # read data from webpage
   webpage_headlines <- xml2::read_html(url_headlines)
 
-  #--- scrap 'country' information from URL
+  
+  
+  # scrap 'country' information ---------------------------------------------
+
   # use CSS selector to scrap countries
   country_html <- rvest::html_nodes(webpage_headlines,
                              paste('.table-group:nth-child(2) ',
                                    '.table-group-item:nth-child(1) ',
                                    'code'))
   # converting countries to text
-  country_data <- rvest::html_text(country_html)
-  # delete erroneous extraction from vector
-  country_data <- country_data[country_data != "sources"]
+  terms_country <- rvest::html_text(country_html)
+  # delete erroneous extraction from vector and change to DF
+  terms_country <- as.data.frame(terms_country[terms_country != "sources"])
+  # rename
+  names(terms_country)[1] <- 'country'
 
+  # scrap 'category' information --------------------------------------------
 
-  #--- scrap 'category' information from URL
   # use CSS selector to scrap categories
   category_html <- rvest::html_nodes(webpage_headlines,
                               paste('.table-group-item:nth-child(2) ',
                                     'code'))
 
   # converting categories to text
-  category_data <- rvest::html_text(category_html)
-  # delete erroneous extraction from vector
-  category_data <- category_data[category_data != "sources"]
+  terms_category <- rvest::html_text(category_html)
+  # delete erroneous extraction from vector and change to DF
+  terms_category <- as.data.frame(terms_category[terms_category != "sources"])
+  # rename
+  names(terms_category)[1] <- 'category'
 
-
-  #--- get sources from newsapi.org
-  sources <- get_sources(api_key)
-
-
-  #--- combine different search terms to list
-
-  searchterms <- list()
-  searchterms[["country"]]   <- country_data
-  searchterms[["category"]]  <- category_data
-  searchterms[["sources"]]   <- sources
-
+  
+  # get 'sources' from newsapi.org ------------------------------------------
+  terms_sources <- get_sources()
+  terms_sources <- terms_sources$results_df["id"]
 
   #--- save list
-  devtools::use_data(searchterms)
+  devtools::use_data(terms_category, terms_country, terms_sources, overwrite = T)
 
 }
