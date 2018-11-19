e30020c81cadaead603a782e9c145ab278d05c49
Yannik Buhl <yannik.buhl@posteo.de>
2018-11-19 08:19:43 +0100

    Committing some minor changes

--- R/get_everything.R
+++ R/get_everything.R
@@ -1,17 +1,18 @@
-#' Get all resources that the API of newsapi.org provides
+#' Get all resources that the API of newsapi.org provides for a given searchterm
 #'
-#' @param api_key Character string with the API key you get from newsapi.org. Passing it is compulsory. Function gets it per default from your global environment. See "set-api-key" for more info.
-#' @param query Character string that contains the content of your search in the API data base
-#' @param page_size The number (numeric!) of articles per page that are returned. Maximum is 100 (default).
+#' @param api_key Character string with the API key you get from newsapi.org. Passing it is compulsory. Function obtains it per default from your global environment. See "set-api-key" for more info.
+#' @param query Character string that contains the searchterm for the API's data base. API supports advanced search parameters, see 'details'. 
+#' @param page_size The number (numeric!) of articles per page that are returned. Maximum is 100 (also default).
 #' @param sources Character string with IDs (comma separated) of the news outlets you want to focus on (e.g., "usa-today, spiegel-online").
-#' @param domains Character string (comma separated) with domains that you want to restrict your search to (e.g. "bbc.com, nytimes.com").
-#' @param exclude_domains Similar usage as 'domains'. Will exclude these domains from your search.
-#' @param from Marks the date of the start of your search. Must be in ISO 8601 format (e.g., '2018-09-08' or '2018-09-08T12:51:42'). Default is the oldest.
-#' @param to Marks the end date of your search. Works similarly as 'from'. Default is the latest article.
+#' @param domains Character string (comma separated) with domains that you want to restrict your search to (e.g., "bbc.com, nytimes.com").
+#' @param exclude_domains Similar usage as with 'domains'. Will exclude these domains from your search.
+#' @param from Marks the start date of your search. Must be in ISO 8601 format (e.g., "2018-09-08" or "2018-09-08T12:51:42"). Default is the oldest available date (depends on your paid/unpaid plan from newsapi.org).
+#' @param to Marks the end date of your search. Works similarly to 'from'. Default is the latest article available.
 #' @param language Specifies the language of the articles of your search. Must be in ISO shortcut format (e.g., "de", "en"). See list of all languages on https://newsapi.org/docs/endpoints/everything. Default is all languages.
-#' @param sort_by Character string that specifies the sorting of your article results. Has three options: "publishedAt", "relevancy", "popularity". Default is "publishedAt".
-#' @param page Specifies the page number of your results. Must be numeric. Default is first page.
-#' @return A list containing a data frame with results for your search and another list with metadata on your search.
+#' @param sort_by Character string that specifies the sorting of your article results. Accepts three options: "publishedAt", "relevancy", "popularity". Default is "publishedAt".
+#' @param page Specifies the page number of your results that is returned. Must be numeric. Default is first page. If you want to get all results at once, use 'get_everything_all' from 'newsanchor'.
+#' @return A list containing 1) a data frame with results for your search and 2) another list with metadata on your search.
+#' @details Advanced search (see also www.newsapi.org): Surround entire phrases with quotes (") for exact matches. Prepend words/phrases that must appear with "+" symbol (e.g., +bitcoin). Prepend words that must not appear with "-" symbol (e.g., -bitcoin). You can also use AND, OR, NOT keywords (optionally grouped with parenthesis, e.g., 'crypto AND (ethereum OR litecoin) NOT bitcoin)').
 #' @examples
 #' \dontrun{
 #' get_everything(api_key = key, content = "stuttgart", language = "de")
@@ -19,7 +20,6 @@
 #' }
 #' @importFrom httr content GET build_url parse_url add_headers
 #' @importFrom jsonlite fromJSON
-#' @return List with two elements: 1) a data frame with search results & 2) list with meta data
 #' @export
 #' 
 
@@ -37,17 +37,13 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
   
   # Initial proceedings -----------------------------------------------------
   
-  # Provide a vector with available ways of sort the articles  
+  # Provide a vector with available ways of sorting the articles  
   sortings <- c("publishedAt", "relevancy", "popularity")
-  
-  # Provide a list of possible languages 
-  ISO <- c("ar", "de", "en", "es", "fr", "he", "it", "nl", "no",
-           "pt", "ru", "se", "ud", "zh")
   
   # Errors and warnings -----------------------------------------------------
   
-  # Make sure a API key is provided
-  if((nchar(api_key) == 0) == TRUE)
+  # Make sure an API key is provided
+  if(nchar(api_key) == 0)
     stop("You did not correctly specify your API key as global variable. See documentation for further info.")
   
   # Make sure that some search term is passed
@@ -56,7 +52,7 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
   
   # Bind together various search parameters as comma-separated strings as required by API
   
-  # Parameter: sources (plus limit to maximum of20 sources)
+  # Parameter: sources (plus limit to maximum of 20 sources)
   if(length(sources) > 20) {
     stop("You cannot select more than 20 sources in one search request.")
   } else {
@@ -89,17 +85,17 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
   
   # Error for non-numeric page parameter
   if(!is.null(page)) {
-    stopifnot(is.numeric(page))#, call. =  FALSE)
-    #stop("Arguments 'page' and 'nrtexts' need to be numeric")
+    stopifnot(is.numeric(page))
   }
   
   # If no page number is specified, set it to value "1"
   if(is.null(page)) {page = 1}
   
   # Error if language indicated does not match the ones provided by the API
-  if(!is.null(language)) {
-    stopifnot((sum(language == ISO) == 1))#, call. = FALSE) # language
-    #stop("Language must be specified as one of these ISO codes: ar, de, en, es, fr, he, it, nl, no, pt, ru, se, ud, zh)
+  if(!is.null(country)){
+    if(!category %in% newsanchor::terms_country$everything) {
+      stop(paste0("Please provide a valid country,", "see data(terms_country)"))
+    }
   }
   
   # Error if selected sorting does not match the ones provided by the API
@@ -132,7 +128,7 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
   content_parsed  <- jsonlite::fromJSON(content_text)
   
   # Check if http status code is valid and construct results accordingly
-  if (results$status_code == 200) {
+  if (results$status_code == 200 & content_parsed$totalResults != 0) {
     
   # Create results data frame
   results_df          <- content_parsed$articles
@@ -160,9 +156,17 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
                    message        = NA)
   
   }
-  
-  # If the http code displays an error, build the results accordingly
-  if (results$status_code != 200) {
+    
+  # If the http code displays an error, throw a warning and build the results accordingly
+  if (results$status_code != 200 | content_parsed$totalResults == 0) {
+    
+    if(results$status_code != 200) {
+      warning(paste0("The search resulted in the following error message:" , content_parsed$message))
+    }
+    
+    if(content_parsed$totalResults == 0) {
+      warning(paste0("The search was not successful. There were no results for your specifications."))
+    }
     
     # Return empty data.frame
     results_df = data.frame()
@@ -174,8 +178,8 @@ get_everything <- function(api_key = Sys.getenv("NEWS_API_KEY"),
                            request_url   = results$url,
                            page_size     = NA,
                            page          = NA,
-                           code          = content_parsed$code,
-                           message       = content_parsed$message)
+                           code          = ifelse(results$status_code != 200, content_parsed$code, NA),
+                           message       = ifelse(results$status_code != 200, content_parsed$message, NA))
   }
   
   # Return results and metadata
--- tests/testthat/test_get_everything.R
+++ tests/testthat/test_get_everything.R
@@ -1,4 +1,9 @@
 context("Get everything")
+
+date_raw <- Sys.Date()
+date1 <- as.character(date_raw - as.difftime(2, unit= "days"))
+date2 <- as.character(date_raw)
+
 testthat::test_that("that that function returns error if no argument provided", {
   testthat::expect_error(newsanchor::get_everything())
 })
@@ -6,14 +11,14 @@ testthat::test_that("that that function returns error if no argument provided", 
 
 testthat::test_that("test that a data frame is returned in the result list.", {
   res <- newsanchor::get_everything(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
-                                    from = "2018-10-10", to = "2018-10-12", 
+                                    from = date1, to = date2, 
                                     query = "Merkel")
   testthat::expect_true(is.data.frame(res$results_df), info = paste0(str(res)))
 })
 
 testthat::test_that("test that the correct number of rows is returned", {
   res <- newsanchor::get_everything(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
-                                    from = "2018-10-10", to = "2018-10-12", 
+                                    from = date1, to = date2, 
                                     query = "Merkel")
   testthat::expect_equal(nrow(res$results_df), 100, 
                          info = paste0("100 rows expected, but only ", nrow(res$results_df), " there."))
@@ -21,7 +26,7 @@ testthat::test_that("test that the correct number of rows is returned", {
 
 testthat::test_that("test that all columns are atomic vectors", {
   res <- newsanchor::get_everything(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
-                                    from = "2018-10-10", to = "2018-10-12", query = "Merkel")
+                                    from = date1, to = date2, query = "Merkel")
   testthat::expect_true(all(sapply(res$results_df, is.atomic)), 
                         info = paste0(colnames(res$results_df)[!sapply(res$results_df, is.atomic)]))
 })
