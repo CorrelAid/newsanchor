d470b58ff4efdefba8fdc54e857cbd349f0aa250
Unknown <friedrike.preu@uni-konstanz.de>
2018-12-09 23:08:20 +0100

    #24 test api key as encrypted travis env variable, more tests

--- tests/testthat/test_get_sources.R
+++ tests/testthat/test_get_sources.R
@@ -5,23 +5,23 @@ testthat::test_that("that that function returns error if no argument provided", 
 })
 
 testthat::test_that("test that the function returns a data frame", {
-  res <- newsanchor::get_sources(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
+  res <- newsanchor::get_sources(api_key = Sys.getenv("NEWS_API_TEST_KEY"), 
                                  category = "general",
                                  language = "de")
   testthat::expect_true(is.data.frame(res$results_df), 
-                        info = paste0(str(res)))
+                        info = paste0(str(res, max.level = 1)))
 })
 
 testthat::test_that("test that all columns are atomic vectors", {
-  res <- newsanchor::get_sources(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
+  res <- newsanchor::get_sources(api_key = Sys.getenv("NEWS_API_TEST_KEY"), 
                                  category = "general",
                                  language = "de")
   testthat::expect_true(all(sapply(res$results_df, is.atomic)),
-                        info = paste0(str(res$results_df)))
+                        info = paste0(str(res$results_df, max.level = 1)))
 })
 
 testthat::test_that("test that the number of rows is greater than zero", {
-  res <- newsanchor::get_sources(api_key = "457e40043f6b4418ab108e3eb11ccc1e", 
+  res <- newsanchor::get_sources(api_key = Sys.getenv("NEWS_API_TEST_KEY"), 
                                     category = "general",
                                     language = "de")
   testthat::expect_gt(nrow(res$results_df), 0)
