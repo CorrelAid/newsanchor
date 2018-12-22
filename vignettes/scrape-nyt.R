0b137b2898cebd390acfe0983d9a061100e78147
Jan Dix <dix.jan@gmx.de>
2018-12-22 16:19:52 +0100

    update NYT vignette

--- vignettes/scrape-nyt.R
+++ vignettes/scrape-nyt.R
@@ -1,4 +1,4 @@
-## ----load packages-------------------------------------------------------
+## ----load packages, warning=FALSE, message=FALSE-------------------------
 # load all required packages
 library(newsanchor) # download newspaper articles
 library(robotstxt)  # get robots.txt
@@ -10,10 +10,10 @@ library(tidytext)   # tidy text analysis
 
 ## ----get meta data, eval=FALSE-------------------------------------------
 #  # get heaadlines published by spiegel online
-#  response <- get_everything(query   = "Trump",
-#                             sources = "the-new-york-times",
-#                             from    = "2018-12-03",
-#                             to      = "2018-12-09")
+#  response <- get_everything_all(query   = "Trump",
+#                                 sources = "the-new-york-times",
+#                                 from    = "2018-12-03",
+#                                 to      = "2018-12-09")
 #  
 #  # extract response data frame
 #  articles <- response$results_df
@@ -21,7 +21,21 @@ library(tidytext)   # tidy text analysis
 ## ----load example data set-----------------------------------------------
 articles <- sample_response$results_df
 
-## ----plot results--------------------------------------------------------
-plot(sentiment ~ date,
-     data = articles_sentiment)
+## ----plot results, dev='svg', fig.align='center', fig.width=7.3, fig.height=4.3----
+# enable two plots in one figure
+old_par <- par(mfrow=c(1, 2))
+
+# plot number of articles vs time 
+barplot(height    = sentiment_by_day$n,
+        names.arg = format(sentiment_by_day$date, "%a"),
+        ylab      = "# of articles",
+        ylim      = c(-10, 35),
+        las       = 2)
+
+# plot sentiment score vs time
+barplot(height    = sentiment_by_day$sentiment,
+        names.arg = format(sentiment_by_day$date, "%a"),
+        ylab      = "Sentiment Score",
+        ylim      = c(-10, 35),
+        las       = 2)
 
